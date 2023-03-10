ff020a925f4504e0af9561740d69abc9fdbfbe7c
hakusa-aizu <soujidecemb@gmail.com>
2023-03-10 16:34:59 +0900

    3/10 17:00 バグあり

--- Minge2023Spring_Team1/Scene.hpp
+++ Minge2023Spring_Team1/Scene.hpp
@@ -27,3 +27,17 @@ public:
 	// 描画関数（オプション）
 	void draw() const override;
 };
+
+class StageSelect : public App::Scene
+{
+public:
+
+	// コンストラクタ（必ず実装）
+	StageSelect(const InitData& init);
+
+	// 更新関数（オプション）
+	void update() override;
+
+	// 描画関数（オプション）
+	void draw() const override;
+};
---
+++ Minge2023Spring_Team1/StageSelect.cpp
@@ -0,0 +1,21 @@
+﻿#include"common.hpp"
+#include"Scene.hpp"
+
+StageSelect::StageSelect(const InitData& init)
+	: IScene{ init }
+{
+
+}
+
+// 更新関数
+void StageSelect::update()
+{
+
+}
+
+// 描画関数
+void StageSelect::draw() const
+{
+	static const Font font(60);
+	font(U"This is StageSelect").drawAt(Scene::Center());
+}
