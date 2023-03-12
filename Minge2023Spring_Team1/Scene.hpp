d745035e91d9ed67bfc13f3d31343663c98dac40
hakusa-aizu <soujidecemb@gmail.com>
2023-03-12 15:35:36 +0900

    draw/update分け、Arrayを使用した一般化

--- Minge2023Spring_Team1/Scene.hpp
+++ Minge2023Spring_Team1/Scene.hpp
@@ -41,6 +41,37 @@ public:
 	// 描画関数（オプション）
 	void draw() const override;
 
+	
+
+private:
+
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
+
+	enum ButtonResponse
+	{
+		normal,
+		endress,
+		other
+	};
+
+	struct ButtonItem
+	{
+		Rect rect;
+		Font font;
+		String text;
+		bool enabled;
+		ButtonResponse response;
+	};
+
+	const Array<ButtonItem> ButtonTable = {
+		{Rect{ 800, 100, 300, 100 }, font, U"ノーマル",   true,  normal},
+		{Rect{ 800, 250, 300, 100 }, font, U"エンドレス", true,  endress},
+		{Rect{ 800, 400, 300, 100 }, font, U"予備",       false, other}
+	};
+
+
 	//ボタン関数
-	bool Button(const Rect& rect, const Font& font, const String& text, bool enabled);
+	bool Button(const Rect& rect, bool enabled);
+	void ButtonDraw(const Rect& rect, const Font& font_, const String& text, bool enabled) const;
+
 };
--- Minge2023Spring_Team1/StageSelect.cpp
+++ Minge2023Spring_Team1/StageSelect.cpp
@@ -1,6 +1,9 @@
 ﻿#include"common.hpp"
 #include"Scene.hpp"
 
+
+
+
 StageSelect::StageSelect(const InitData & init)
 	: IScene{ init }
 {
@@ -11,56 +14,70 @@ StageSelect::StageSelect(const InitData & init)
 // 更新関数
 void StageSelect::update()
 {
-	static const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
+	
+	for (size_t i = 0; i < ButtonTable.size(); ++i) {
 
+		if (Button(ButtonTable[i].rect, ButtonTable[i].enabled))
+		{
+			switch (ButtonTable[i].response)
+			{
+			case normal :
+				Print << U"A";
+				break;
 
-	if (Button(Rect{ 800, 100, 300, 100 }, font, U"ノーマル", true))
-	{
-		Print << U"A";
+			case endress :
+				Print << U"B";
+				break;
+
+			default:
+				Print << U"C";
+				break;
+			}
+		}
 	}
 
-	if (Button(Rect{ 800, 250, 300, 100 }, font, U"エンドレス", true))
-	{
-		Print << U"B";
-	}
-
-	if (Button(Rect{ 800, 400, 300, 100 }, font, U"予備", false))
-	{
-		Print << U"C";
-	}
 
 }
 
 // 描画関数
 void StageSelect::draw() const
 {
-	static const Font font(60);
-	//font(U"This is StageSelect").drawAt(Scene::Center());
+	for (size_t i = 0; i < ButtonTable.size(); ++i) {
 
+		ButtonDraw(ButtonTable[i].rect, ButtonTable[i].font, ButtonTable[i].text, ButtonTable[i].enabled);
+
+	}
 }
 
 //ボタン関数
-bool StageSelect::Button(const Rect& rect, const Font& font, const String& text, bool enabled)
+bool StageSelect::Button(const Rect& rect, bool enabled)
 {
 	if (enabled && rect.mouseOver())
 	{
 		Cursor::RequestStyle(CursorStyle::Hand);
 	}
 
+	return (enabled && rect.leftClicked());
+}
+
+//ボタン関数
+void StageSelect::ButtonDraw(const Rect& rect, const Font& font_, const String& text, bool enabled) const
+{
+	
 	if (enabled)
 	{
 		rect.draw(ColorF{ 0.3, 0.7, 1.0 });
 		rect.drawFrame(2, 2, ColorF{ 0.5, 0.7, 1.0 });
 
-		font(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2));
+		font_(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2));
 	}
 	else
 	{
 		rect.draw(ColorF{ 0.5 });
 		rect.drawFrame(2, 2, ColorF{ 0.5 });
 
-		font(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2), ColorF{ 0.7 });
+		font_(text).drawAt(40, (rect.x + rect.w / 2), (rect.y + rect.h / 2), ColorF{ 0.7 });
 	}
-
-	return (enabled && rect.leftClicked());
 }
+
+
